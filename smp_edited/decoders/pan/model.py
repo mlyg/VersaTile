104d4b9032af56b7eefb85d4b1a5f9c6ec9c2cb0
mlyg <imichaelyeung@gmail.com>
2023-08-31 16:37:21 +0100

    Added SMP edited code

--- smp_edited/decoders/pan/model.py
+++ smp_edited/decoders/pan/model.py
@@ -1,4 +1,4 @@
-from typing import Optional, Union, List
+from typing import Optional, Union
 
 from segmentation_models_pytorch.encoders import get_encoder
 from segmentation_models_pytorch.base import (
@@ -6,38 +6,31 @@ from segmentation_models_pytorch.base import (
     SegmentationHead,
     ClassificationHead,
 )
-from .decoder import MAnetDecoder
+from .decoder import PANDecoder
 
 
-class MAnet(SegmentationModel):
-    """MAnet_ :  Multi-scale Attention Net. The MA-Net can capture rich contextual dependencies based on
-    the attention mechanism, using two blocks:
-     - Position-wise Attention Block (PAB), which captures the spatial dependencies between pixels in a global view
-     - Multi-scale Fusion Attention Block (MFAB), which  captures the channel dependencies between any feature map by
-       multi-scale semantic feature fusion
+class PAN(SegmentationModel):
+    """Implementation of PAN_ (Pyramid Attention Network).
+
+    Note:
+        Currently works with shape of input tensor >= [B x C x 128 x 128] for pytorch <= 1.1.0
+        and with shape of input tensor >= [B x C x 256 x 256] for pytorch == 1.3.1
 
     Args:
         encoder_name: Name of the classification model that will be used as an encoder (a.k.a backbone)
             to extract features of different spatial resolution
-        encoder_depth: A number of stages used in encoder in range [3, 5]. Each stage generate features
-            two times smaller in spatial dimensions than previous one (e.g. for depth 0 we will have features
-            with shapes [(N, C, H, W),], for depth 1 - [(N, C, H, W), (N, C, H // 2, W // 2)] and so on).
-            Default is 5
         encoder_weights: One of **None** (random initialization), **"imagenet"** (pre-training on ImageNet) and
             other pretrained weights (see table with available weights for each encoder_name)
-        decoder_channels: List of integers which specify **in_channels** parameter for convolutions used in decoder.
-            Length of the list should be the same as **encoder_depth**
-        decoder_use_batchnorm: If **True**, BatchNorm2d layer between Conv2D and Activation layers
-            is used. If **"inplace"** InplaceABN will be used, allows to decrease memory consumption.
-            Available options are **True, False, "inplace"**
-        decoder_pab_channels: A number of channels for PAB module in decoder.
-            Default is 64.
+        encoder_output_stride: 16 or 32, if 16 use dilation in encoder last layer.
+            Doesn't work with ***ception***, **vgg***, **densenet*`** backbones.Default is 16.
+        decoder_channels: A number of convolution layer filters in decoder blocks
         in_channels: A number of input channels for the model, default is 3 (RGB images)
         classes: A number of classes for output mask (or you can think as a number of channels of output mask)
         activation: An activation function to apply after the final convolution layer.
             Available options are **"sigmoid"**, **"softmax"**, **"logsoftmax"**, **"tanh"**, **"identity"**,
                 **callable** and **None**.
             Default is **None**
+        upsampling: Final upsampling factor. Default is 4 to preserve input-output spatial shape identity
         aux_params: Dictionary with parameters of the auxiliary output (classification head). Auxiliary output is build
             on top of encoder if **aux_params** is not **None** (default). Supported params:
                 - classes (int): A number of classes
@@ -47,55 +40,57 @@ class MAnet(SegmentationModel):
                     (could be **None** to return logits)
 
     Returns:
-        ``torch.nn.Module``: **MAnet**
+        ``torch.nn.Module``: **PAN**
 
-    .. _MAnet:
-        https://ieeexplore.ieee.org/abstract/document/9201310
+    .. _PAN:
+        https://arxiv.org/abs/1805.10180
 
     """
 
     def __init__(
         self,
         encoder_name: str = "resnet34",
-        encoder_depth: int = 5,
         encoder_weights: Optional[str] = "imagenet",
-        decoder_use_batchnorm: bool = True,
-        decoder_channels: List[int] = (256, 128, 64, 32, 16),
-        decoder_pab_channels: int = 64,
+        encoder_output_stride: int = 16,
+        decoder_channels: int = 32,
         in_channels: int = 3,
         classes: int = 1,
         activation: Optional[Union[str, callable]] = None,
+        upsampling: int = 4,
         aux_params: Optional[dict] = None,
     ):
         super().__init__()
 
+        if encoder_output_stride not in [16, 32]:
+            raise ValueError("PAN support output stride 16 or 32, got {}".format(encoder_output_stride))
+
         self.encoder = get_encoder(
             encoder_name,
             in_channels=in_channels,
-            depth=encoder_depth,
+            depth=5,
             weights=encoder_weights,
+            output_stride=encoder_output_stride,
         )
 
-        self.decoder = MAnetDecoder(
+        self.decoder = PANDecoder(
             encoder_channels=self.encoder.out_channels,
             decoder_channels=decoder_channels,
-            n_blocks=encoder_depth,
-            use_batchnorm=decoder_use_batchnorm,
-            pab_channels=decoder_pab_channels,
         )
 
         self.segmentation_head_sem = SegmentationHead(
-            in_channels=decoder_channels[-1],
+            in_channels=decoder_channels,
             out_channels=classes,
             activation=activation,
             kernel_size=3,
+            upsampling=upsampling,
         )
 
         self.segmentation_head_dtm = SegmentationHead(
-            in_channels=decoder_channels[-1],
+            in_channels=decoder_channels,
             out_channels=classes,
             activation=activation,
             kernel_size=3,
+            upsampling=upsampling,
         )
 
         if aux_params is not None:
@@ -103,5 +98,5 @@ class MAnet(SegmentationModel):
         else:
             self.classification_head = None
 
-        self.name = "manet-{}".format(encoder_name)
+        self.name = "pan-{}".format(encoder_name)
         self.initialize()
